7b9717c56aac942715ff2cedbd298205ed8ffaea
fengchaolin <linfengchao@taqu.cn>
2019-09-12 11:30:20 +0800

    1.使用代码完善

--- lib/line_chart_1.dart
+++ lib/line_chart_1.dart
@@ -8,6 +8,8 @@ import 'package:flutter_chart/chart2/linechart/line_chart_data.dart';
 import 'chart2/fl_chart.dart';
 
 class LineChartStyle1 extends StatefulWidget {
+  static final String sName = "LineChartStyle1";
+
   @override
   _LinChartStyle1 createState() {
     return _LinChartStyle1();
@@ -22,8 +24,10 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
   bool isOpenTouchEvent = true;
   bool isShowTopValue = true;
   bool isShowIndicator = true;
-
-
+  bool isShowLineValue = true;
+  bool isShowLineDot = true;
+  List<HorizontalLimitLine> limitLine = [];
+  double num = 30;
 
   @override
   void initState() {
@@ -31,6 +35,14 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
     lineMode = LineMode.CUBIC_BEZIER;
     isStrokeCapRound = true;
     lineColors = [Colors.blue];
+    limitLine.add(HorizontalLimitLine(
+        y: 25,
+        text: "上周平均值25%",
+        textMargin: 0,
+        textStyle: TextStyle(color: Colors.red, fontSize: 8),
+        limitAlignment: HorizontalLimitAlignment.TOP_LEFT,
+        color: Colors.red,
+        strokeWidth: 0.5));
   }
 
   @override
@@ -76,6 +88,7 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
                           lineWidth: 4,
                           lineColors: [Colors.blue],
                           lineDotStyle: LineDotStyle(
+                              show: isShowLineDot,
                               isStroke: true,
                               strokeWidth: 4,
                               dotColor: Colors.blue,
@@ -87,13 +100,12 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
                           lineFillStyle: LineFillStyle(
                               show: true, colors: [Color(0x66E3F2FD)]),
                           chartValueStyle: ChartValueStyle(
-                              show: true,
+                              show: isShowLineValue,
                               textStyle:
                                   TextStyle(color: Colors.blue, fontSize: 10),
                               valueFormat: ((ChartPoint point) {
                                 return point.y.toInt().toString() + "%";
                               }))),
-
                       LineChartBarData(
                           spots: [
                             ChartPoint(1, 0),
@@ -109,10 +121,11 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
                           lineWidth: 4,
                           lineColors: [Colors.red],
                           lineDotStyle: LineDotStyle(
-                              isStroke: true,
+                              show: isShowLineDot,
+                              isStroke: false,
                               strokeWidth: 4,
                               dotColor: Colors.red,
-                              dotSize: 4,
+                              dotSize: 5,
                               //可以过滤掉一些不需要显示的点
                               checkToShowDot: ((ChartPoint spot) {
                                 return true;
@@ -120,9 +133,9 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
                           lineFillStyle: LineFillStyle(
                               show: true, colors: [Color(0x66E57373)]),
                           chartValueStyle: ChartValueStyle(
-                              show: true,
+                              show: isShowLineValue,
                               textStyle:
-                              TextStyle(color: Colors.red, fontSize: 10),
+                                  TextStyle(color: Colors.red, fontSize: 10),
                               valueFormat: ((ChartPoint point) {
                                 return point.y.toInt().toString() + "%";
                               }))),
@@ -150,32 +163,23 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
                         chartLegendForm: ChartLegendForm.CIRCLE,
                         chartLegendAlignment: ChartLegendAlignment.RIGHT,
                         chartLegendLocation: ChartLegendLocation.BOTTOM,
-                        legendText: ["每日比增","上周每日"]),
+                        legendText: ["每日比增", "上周每日"]),
                     lineChartTouchStyle: LineChartTouchStyle(
-                        enable: isOpenTouchEvent,//关闭触摸响应
+                        enable: isOpenTouchEvent,
+                        //关闭触摸响应
                         isShowIndicator: isShowIndicator,
                         indicatorColor: Colors.black,
                         indicatorWidth: 0.5,
                         touchTopTicStyle: TouchTopTicStyle(
                             show: isShowTopValue,
                             topTipMargin: 5,
-                            topTipEdgInsets: EdgeInsets.all(6),getTopTipText: (ChartPoint point){
-                              return "y = "+point.y.toInt().toString();
-                        })),
+                            topTipEdgInsets: EdgeInsets.all(6),
+                            getTopTipText: (ChartPoint point) {
+                              return "y = " + point.y.toInt().toString();
+                            })),
                     limitLineData: LimitLineData(
                         showHorizontalLines: true,
-                        horizontalLines: [
-                          HorizontalLimitLine(
-                              y: 25,
-                              text: "上周平均值25%",
-                              textMargin: 0,
-                              textStyle:
-                                  TextStyle(color: Colors.red, fontSize: 8),
-                              limitAlignment: HorizontalLimitAlignment.TOP_LEFT,
-                              color: Colors.red,
-                              strokeWidth: 0.5)
-                        ],
-                        showVerticalLines: false)))),
+                        horizontalLines: limitLine)))),
           ),
           Row(
             children: <Widget>[
@@ -239,35 +243,112 @@ class _LinChartStyle1 extends State<LineChartStyle1> {
                     height: 35,
                     child: Center(
                       child: Text(
-                        isShowTopValue ? "关闭头部value" : "显示头部value",
+                        isShowTopValue ? "关闭点击提示" : "显示点击提示",
                         style: TextStyle(color: Colors.white, fontSize: 14),
                       ),
                     )),
               )
             ],
           ),
-          Row(children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  isShowIndicator = !isShowIndicator;
-                });
-              },
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
-                  margin: EdgeInsets.only(top: 16, left: 16),
-                  width: 110,
-                  height: 35,
-                  child: Center(
-                    child: Text(
-                      isShowIndicator ? "关闭触摸指示器" : "打开触摸指示器",
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                  )),
-            )
-          ],)
+          Row(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isShowIndicator = !isShowIndicator;
+                  });
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    margin: EdgeInsets.only(top: 16, left: 16),
+                    width: 110,
+                    height: 35,
+                    child: Center(
+                      child: Text(
+                        isShowIndicator ? "关闭触摸指示器" : "打开触摸指示器",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    )),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isShowLineValue = !isShowLineValue;
+                  });
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    margin: EdgeInsets.only(top: 16, left: 16),
+                    width: 110,
+                    height: 35,
+                    child: Center(
+                      child: Text(
+                        isShowLineValue ? "关闭线上value" : "显示value",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    )),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isShowLineDot = !isShowLineDot;
+                  });
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    margin: EdgeInsets.only(top: 16, left: 16),
+                    width: 90,
+                    height: 35,
+                    child: Center(
+                      child: Text(
+                        isShowLineDot ? "关闭点" : "显示点",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    )),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  if(num >60){
+                    return;
+                  }
+                  setState(() {
+                    num+=5;
+                    limitLine.add(HorizontalLimitLine(
+                        y: num,
+                        text: "新线"+num.toString() +"%",
+                        textMargin: 0,
+                        textStyle: TextStyle(color: Colors.red, fontSize: 8),
+                        limitAlignment: HorizontalLimitAlignment.TOP_LEFT,
+                        color: Colors.red,
+                        strokeWidth: 0.5));
+                  });
+                },
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    margin: EdgeInsets.only(top: 16, left: 16),
+                    width: 90,
+                    height: 35,
+                    child: Center(
+                      child: Text(
+                        "添加限制线",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    )),
+              )
+            ],
+          )
         ]));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: LineChartStyle1(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: <String, WidgetBuilder>{
+        LineChartStyle1.sName: (context) => LineChartStyle1(),
+      },
     );
   }
 }
-
-
-
-
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -52,201 +49,31 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Container(
-              width: 300,
-              height: 300,
-              child: FlutterChart(
-                chart: LineChart(LineChartData(
-                  limitLineData: LimitLineData(
-                      showHorizontalLines: true,
-                      horizontalLines: [
-                        HorizontalLimitLine(
-                            y: 2.5,
-                            color: Colors.deepPurpleAccent,
-                            strokeWidth: 1,
-                            text: "限制线")
-                      ],
-                      showVerticalLines: true,
-                      verticalLines: [
-                        VerticalLimitLine(
-                            x: 8,
-                            color: Colors.deepPurpleAccent,
-                            strokeWidth: 1)
-                      ]),
-                  chartLegendStyle: ChartLegendStyle(
-                      showLegend: true,
-                      textStyle: new TextStyle(color: Colors.red, fontSize: 12),
-                      legendText: ["测试1", "测试二"]),
-                  titlesStyle: ChartTitlesStyle(
-                      show: true,
-                      leftTitles: TitlesStyle(
-                        showTitles: true,
-                        reservedSize: 22,
-                        textStyle: TextStyle(
-                            color: const Color(0xff68737d), fontSize: 10),
-                        getTitlesFormat: (value) {
-                          return "${value.toInt()}K";
-                        },
-                        margin: 8,
-                      ),
-                      bottomTitles: TitlesStyle(
-                        showTitles: true,
-                        reservedSize: 22,
-                        textStyle: TextStyle(
-                            color: const Color(0xff68737d), fontSize: 10),
-                        getTitlesFormat: (value) {
-                          return "${value.toInt()}";
-                        },
-                        margin: 8,
-                      ),
-                      rightTitles: TitlesStyle(
-                        showTitles: true,
-                        reservedSize: 22,
-                        textStyle: TextStyle(
-                            color: const Color(0xff68737d), fontSize: 10),
-                        getTitlesFormat: (value) {
-                          return "${value.toInt()}";
-                        },
-                        margin: 8,
-                      ),
-                      topTitles: TitlesStyle(
-                        showTitles: true,
-                        reservedSize: 22,
-                        textStyle: TextStyle(
-                            color: const Color(0xff68737d), fontSize: 10),
-                        getTitlesFormat: (value) {
-                          return "${value.toInt()}";
-                        },
-                        margin: 8,
-                      )),
-                  gridData: ChartGridStyle(
-                      show: true,
-                      drawHorizontalGrid: true,
-                      getHorizontalGridLine: gridLine2,
-                      checkToShowHorizontalGrid: (double value) {
-                        return value != 0;
-                      },
-                      drawVerticalGrid: true,
-                      getVerticalGridLine: gridLine,
-                      checkToShowVerticalGrid: (double value) {
-                        return value != 0;
-                      }),
-                  borderStyle: ChartBorderStyle(
-                      show: true,
-                      border: Border.all(
-                          color: Colors.yellow,
-                          width: 1.0,
-                          style: BorderStyle.solid)),
-                  minX: 0,
-                  maxX: 13,
-                  minY: 0,
-                  maxY: 5,
-//                   backgroundColor: Colors.grey,
-                  lineBarsData: [
-                    LineChartBarData(
-                        chartValueStyle: ChartValueStyle(
-                            show: true,
-                            textStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: 10,
-                                fontWeight: FontWeight.bold),
-                            margin: 6,
-                            valueFormat: (ChartPoint point) {
-                              return point.y.toInt().toString() + " k";
-                            }),
-                        spots: [
-                          ChartPoint(0, 0),
-                          ChartPoint(1, 1),
-                          ChartPoint(3, 1.5),
-                          ChartPoint(5, 1.4),
-                          ChartPoint(7, 3.4),
-                          ChartPoint(10, 2),
-                          ChartPoint(12, 2.2),
-                          ChartPoint(13, 1.8),
-                        ],
-                        lineMode: LineMode.LINEAR,
-                        isStrokeCapRound: false,
-                        lineColors: [Colors.red],
-                        lineWidth: 10,
-                        intensity: 2,
-                        lineDotStyle: LineDotStyle(
-                            isStroke: false,
-                            dotColor: Colors.black,
-                            dotSize: 5),
-                        lineFillStyle: LineFillStyle(show: true, colors: [
-                          Color(0xFFE57373),
-                        ])),
-                    LineChartBarData(
-                        spots: [
-                          ChartPoint(0, 0),
-                          ChartPoint(1, 2),
-                          ChartPoint(3, 2.5),
-                          ChartPoint(5, 2.4),
-                          ChartPoint(7, 4.4),
-                          ChartPoint(10, 3),
-                          ChartPoint(12, 3.2),
-                          ChartPoint(13, 2.8),
-                        ],
-                        lineMode: LineMode.CUBIC_BEZIER,
-                        isStrokeCapRound: true,
-                        lineColors: [
-                          Colors.blue,
-                          Colors.amberAccent,
-                          Colors.green
-                        ],
-                        lineWidth: 5,
-                        intensity: 0.2,
-                        lineDotStyle: LineDotStyle(
-                            isStroke: true,
-                            dotColor: Colors.black,
-                            dotSize: 4,
-                            strokeWidth: 2),
-                        lineFillStyle: LineFillStyle(show: true, colors: [
-                          Color(0x8023b6e6),
-                          Color(0x8802d39a),
-                        ]))
-                  ],
-                )),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, LineChartStyle1.sName);
+              },
+              child: Container(
+                height: 52,
+                decoration: BoxDecoration(
+                    color: Colors.blue, borderRadius: BorderRadius.circular(8)),
+                margin: EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    '线性图表样式1',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
-  ChartLine gridLine(double value) {
-    return const ChartLine(
-      color: Colors.grey,
-      strokeWidth: 1,
-    );
-  }
-
-  ChartLine gridLine2(double value) {
-    return const ChartLine(
-      color: Colors.red,
-      strokeWidth: 1,
-    );
-  }
-
-  List<Color> gradientColors = [
-    Color(0xff23b6e6),
-    Color(0xff02d39a),
-  ];
 }
